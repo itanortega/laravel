8000febce48f9c8ecf16a5505cbbbefed4ffc64a
Itan Ortega <io@MacBook-Air-de-Itan.local>
2017-12-31 09:36:38 -0500

    paginado

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,7 @@ use App\Message;
 class PagesController extends Controller
 {
     public function home() {
-        $messages = Message::all();
+        $messages = Message::paginate(10);
         return view('welcome', [
             'messages' => $messages
         ]);
